e1a54f7a5b004337e4e94ac46ea28530144646d8
Timo Ruohomaki <timo.ruohomaki@forumvirium.fi>
2021-12-19 21:09:15 +0200

    week 4

---
+++ src/005/go.mod
@@ -0,0 +1,3 @@
+module github.com/timoruohomaki/Training01/tree/master/src/005
+
+go 1.17
--- src/005/makejson.go
+++ src/005/makejson.go
@@ -1,7 +1,45 @@
+/*
+Write a program which prompts the user to first enter a name, and then enter an address.
+Your program should create a map and add the name and address to the map using the keys
+“name” and “address”, respectively. Your program should use Marshal() to create a JSON
+object from the map, and then your program should print the JSON object.
+*/
+
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var name string
+var address string
 
 func main() {
-	fmt.Println(" ")
+	fmt.Println("Enter name:")
+	_, err1 := fmt.Scanln(&name)
+	if err1 != nil {
+		panic(err1)
+	}
+	fmt.Println("Enter address:")
+	_, err2 := fmt.Scanln(&address)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	pMap := make(map[string]string)
+
+	pMap["name"] = name
+	pMap["address"] = address
+
+	barr, err3 := json.Marshal(pMap)
+
+	if err3 != nil {
+		panic(err3)
+	}
+
+	fmt.Println("")
+	fmt.Println("Thank you. Person object in JSON notation is: ")
+	fmt.Println("")
+	fmt.Println(string(barr))
 }
